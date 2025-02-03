250346f8e375d67f6634840698287dd4b3e85c4d
M. Anas Aijaz <127969451+manasajaz@users.noreply.github.com>
2025-02-03 21:30:09 +0500

    Update home.jsx
    change the code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Sidepannel from "../components/side-pannel";
+import doctor from "../assets/doctor.png"; 
+import patient from "../assets/patient.png"; 
+
 
 function Home() {
+  const [doctors, setDoctors] = useState([]);
+  const [patients, setPatients] = useState([]);
+
+  // Fetch doctors & patients from API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [doctorRes, patientRes] = await Promise.all([
+          fetch("http://localhost:4600/api/auth/doctor-card"),
+          fetch("http://localhost:4600/api/auth/patient-card"),
+        ]);
+
+        const doctorsData = await doctorRes.json();
+        const patientsData = await patientRes.json();
+
+        setDoctors(doctorsData); // Show all doctors dynamically
+        setPatients(patientsData); // Show all patients dynamically
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Runs once on mount
+
   return (
     <div className="layout-wrapper layout-content-navbar">
       <div className="layout-container">
@@ -13,215 +41,34 @@ function Home() {
           <div className="content-wrapper">
             <div className="container-xxl flex-grow-1 container-p-y">
               <div className="row">
-                <div className="col-lg-6 col-md-6">
-                  <div className="row">
-                    <div className="col-lg-6 col-md-12 col-6 mb-4">
-                      <div className="card">
-                        <div className="card-body">
-                          <div className="card-title d-flex align-items-start justify-content-between">
-                            <div className="avatar flex-shrink-0">
-                              <img
-                                src="../assets/img/icons/unicons/chart-success.png"
-                                alt="chart success"
-                                className="rounded"
-                              />
-                            </div>
-                            <div className="dropdown">
-                              <button
-                                className="btn p-0"
-                                type="button"
-                                id="cardOpt3"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i className="bx bx-dots-vertical-rounded" />
-                              </button>
-                              <div
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="cardOpt3"
-                              >
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  View More
-                                </a>
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  Delete
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                          <span className="fw-semibold d-block mb-1">
-                            Profit
-                          </span>
-                          <h3 className="card-title mb-2">$12,628</h3>
-                          <small className="text-success fw-semibold">
-                            <i className="bx bx-up-arrow-alt" /> +72.80%
-                          </small>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-6 col-md-12 col-6 mb-4">
-                      <div className="card">
-                        <div className="card-body">
-                          <div className="card-title d-flex align-items-start justify-content-between">
-                            <div className="avatar flex-shrink-0">
-                              <img
-                                src="../assets/img/icons/unicons/wallet-info.png"
-                                alt="Credit Card"
-                                className="rounded"
-                              />
-                            </div>
-                            <div className="dropdown">
-                              <button
-                                className="btn p-0"
-                                type="button"
-                                id="cardOpt6"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i className="bx bx-dots-vertical-rounded" />
-                              </button>
-                              <div
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="cardOpt6"
-                              >
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  View More
-                                </a>
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  Delete
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                          <span>Sales</span>
-                          <h3 className="card-title text-nowrap mb-1">
-                            $4,679
-                          </h3>
-                          <small className="text-success fw-semibold">
-                            <i className="bx bx-up-arrow-alt" /> +28.42%
-                          </small>
-                        </div>
-                      </div>
+                {/* Doctors Count Card */}
+                <div className="col-lg-6 col-md-6 col-12 mb-4">
+                  <div className="card text-center p-4 shadow-sm">
+                    <div className="card-body">
+                      <img
+                        src={doctor}
+                        alt="Doctors"
+                        className="mb-3 mx-auto"
+                        style={{ width: "50px" }}
+                      />
+                      <h4 className="card-title">Total Doctors</h4>
+                      <h2 className="text-primary">{doctors.length}</h2>
                     </div>
                   </div>
                 </div>
-                <div className="col-12 col-md-6 col-lg-6">
-                  <div className="row">
-                    <div className="col-6 mb-4">
-                      <div className="card">
-                        <div className="card-body">
-                          <div className="card-title d-flex align-items-start justify-content-between">
-                            <div className="avatar flex-shrink-0">
-                              <img
-                                src="../assets/img/icons/unicons/paypal.png"
-                                alt="Credit Card"
-                                className="rounded"
-                              />
-                            </div>
-                            <div className="dropdown">
-                              <button
-                                className="btn p-0"
-                                type="button"
-                                id="cardOpt4"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i className="bx bx-dots-vertical-rounded" />
-                              </button>
-                              <div
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="cardOpt4"
-                              >
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  View More
-                                </a>
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  Delete
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                          <span className="d-block mb-1">Payments</span>
-                          <h3 className="card-title text-nowrap mb-2">
-                            $2,456
-                          </h3>
-                          <small className="text-danger fw-semibold">
-                            <i className="bx bx-down-arrow-alt" /> -14.82%
-                          </small>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-6 mb-4">
-                      <div className="card">
-                        <div className="card-body">
-                          <div className="card-title d-flex align-items-start justify-content-between">
-                            <div className="avatar flex-shrink-0">
-                              <img
-                                src="../assets/img/icons/unicons/cc-primary.png"
-                                alt="Credit Card"
-                                className="rounded"
-                              />
-                            </div>
-                            <div className="dropdown">
-                              <button
-                                className="btn p-0"
-                                type="button"
-                                id="cardOpt1"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i className="bx bx-dots-vertical-rounded" />
-                              </button>
-                              <div
-                                className="dropdown-menu"
-                                aria-labelledby="cardOpt1"
-                              >
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  View More
-                                </a>
-                                <a
-                                  className="dropdown-item"
-                                  href="javascript:void(0);"
-                                >
-                                  Delete
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                          <span className="fw-semibold d-block mb-1">
-                            Transactions
-                          </span>
-                          <h3 className="card-title mb-2">$14,857</h3>
-                          <small className="text-success fw-semibold">
-                            <i className="bx bx-up-arrow-alt" /> +28.14%
-                          </small>
-                        </div>
-                      </div>
+
+                {/* Patients Count Card */}
+                <div className="col-lg-6 col-md-6 col-12 mb-4">
+                  <div className="card text-center p-4 shadow-sm">
+                    <div className="card-body">
+                      <img
+                        src={patient}
+                        alt="Patients"
+                        className="mb-3 mx-auto"
+                        style={{ width: "50px" }}
+                      />
+                      <h4 className="card-title">Total Patients</h4>
+                      <h2 className="text-success">{patients.length}</h2>
                     </div>
                   </div>
                 </div>
